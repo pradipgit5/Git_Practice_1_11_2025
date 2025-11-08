34983cdeac256dc869923a803cf65a119b4ebf21
pradipgit5 <pradipgupta@gmail.com>
2025-11-08 12:57:07 +0530

    Reapply "Commit-4"
    This reverts commit bb5b0d3f745555e5762c2ea8bc1f4ba68aea243b.

--- GitPractice_Project/Controllers/WeatherForecastController.cs
+++ GitPractice_Project/Controllers/WeatherForecastController.cs
@@ -35,6 +35,10 @@ namespace GitPractice_Project.Controllers
             Console.WriteLine("This is the example of revert logic");
         }
 
+        public void Commit_4() {
+            Console.WriteLine("This is the example of commit-4");
+        }
+
         public void Commit_6()
         {
             Console.WriteLine("This is example of commit-6");
