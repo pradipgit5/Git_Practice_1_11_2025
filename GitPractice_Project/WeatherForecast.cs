71280a357e8da05b217ddce1e4e93056bf6551bf
pradipgit5 <pradipgupta@gmail.com>
2025-12-16 11:53:15 +0530

    revert commit1

--- GitPractice_Project/WeatherForecast.cs
+++ GitPractice_Project/WeatherForecast.cs
@@ -9,5 +9,11 @@ namespace GitPractice_Project
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+
+        public void revert_Commit1()
+        {
+            Console.WriteLine("This is a new method added to demonstrate revert commit1  functionality.");
+        }
     }
 }
